ee7a9f4c89da3ce26858d773f1ad1256eae85dcb
nokizorque <lewis.watson@outlook.com>
2015-08-22 15:23:41 +1000

    [CSGstaff] Misc

--- [admin]/CSGstaff/warnings.lua
+++ [admin]/CSGstaff/warnings.lua
@@ -1,50 +1,52 @@
 -- Nickchange handler
 addEventHandler("onPlayerChangeNick", root,
-	function ( oldNick, newNick )
-		for k, thePlayer in pairs( getOnlineAdmins () ) do
-			exports.killmessages:outputMessage( oldNick.." is now known as "..newNick, thePlayer, 225, 0, 0)
+	function (oldNick, newNick)
+		for k, thePlayer in pairs(getOnlineAdmins()) do
+			exports.killmessages:outputMessage(oldNick.." is now known as "..newNick, thePlayer, 225, 0, 0)
+			-- Since killmessages don't output to the console, let's do it
+			outputConsole(oldNick.." is now known as "..newNick, thePlayer)
 		end
 	end
 )
 
 -- Output a staff message
-function createNewAdminDxMessage ( theMessage, r, g, b )
-	for k, thePlayer in pairs( getOnlineAdmins () ) do
-		exports.killmessages:outputMessage( theMessage, thePlayer, r, g, b )
+function createNewAdminDxMessage(theMessage, r, g, b)
+	for k, thePlayer in pairs(getOnlineAdmins()) do
+		exports.killmessages:outputMessage(theMessage, thePlayer, r, g, b)
 	end
 end
 
 -- Deathmatch warns
-addEventHandler( "onPlayerWasted", root,
-	function ( ammo, attacker, weapon, bodypart )
-		if ( attacker ) and not ( source == attacker ) then
-			if ( getElementType ( attacker ) == "vehicle" ) then
-				if isElement(getVehicleController( attacker )) then 
-					attacker = getVehicleController( attacker ) 
+addEventHandler("onPlayerWasted", root,
+	function (ammo, attacker, weapon, bodypart)
+		if (attacker) and not (source == attacker) then
+			if (getElementType (attacker) == "vehicle") then
+				if isElement(getVehicleController(attacker)) then 
+					attacker = getVehicleController(attacker) 
 				else
 					return false
 				end
 			end
-			local sourceTeam = getPlayerTeam( source )
-			local attackTeam = getPlayerTeam( attacker )
+			local sourceTeam = getPlayerTeam(source)
+			local attackTeam = getPlayerTeam(attacker)
 			if exports.server:getPlayChatZone(attacker) ~= "LV" and attackTeam and sourceTeam then
 				local sourceTeam = getTeamName(sourceTeam)
 				local attackTeam = getTeamName(attackTeam)
-				if getElementDimension( source ) ~= 20 and getElementDimension( source ) ~= 125 and attackTeam ~= "Staff" then
-					if ( attackTeam == "SWAT" ) or ( attackTeam == "Police" ) or ( attackTeam == "Military Forces" ) or ( attackTeam == "Government Agency" ) then
-						if getElementData( source, "wantedPoints" ) < 1 then
-							createNewAdminDxMessage( getPlayerName( attacker ).." possibly deathmatched "..getPlayerName( source ), 225, 0, 0 )
+				if getElementDimension(source) ~= 20 and getElementDimension(source) ~= 125 and attackTeam ~= "Staff" then
+					if (attackTeam == "SWAT") or (attackTeam == "Police") or (attackTeam == "Military Forces") or (attackTeam == "Government Agency") then
+						if getElementData(source, "wantedPoints") < 1 then
+							createNewAdminDxMessage(getPlayerName(attacker).." possibly deathmatched "..getPlayerName(source), 225, 0, 0)
 						end
 					elseif attackTeam == "Criminals" then
-						if getElementData( attacker, "wantedPoints" ) < 1 then
-							createNewAdminDxMessage( getPlayerName( attacker ).." possibly deathmatched "..getPlayerName( source ), 225, 0, 0 )
-						elseif getElementData( attacker, "wantedPoints" ) >= 1 then
-							if ( sourceTeam ~= "SWAT" ) and ( sourceTeam ~= "Police" ) and ( sourceTeam ~= "Military Forces" ) and ( sourceTeam ~= "Government Agency" ) then
-								createNewAdminDxMessage( getPlayerName( attacker ).." possibly deathmatched "..getPlayerName( source ), 225, 0, 0 )
+						if getElementData(attacker, "wantedPoints") < 1 then
+							createNewAdminDxMessage(getPlayerName(attacker).." possibly deathmatched "..getPlayerName(source), 225, 0, 0)
+						elseif getElementData(attacker, "wantedPoints") >= 1 then
+							if (sourceTeam ~= "SWAT") and (sourceTeam ~= "Police") and (sourceTeam ~= "Military Forces") and (sourceTeam ~= "Government Agency") then
+								createNewAdminDxMessage(getPlayerName(attacker).." possibly deathmatched "..getPlayerName(source), 225, 0, 0)
 							end
 						end
 					else
-						createNewAdminDxMessage( getPlayerName( attacker ).." possibly deathmatched "..getPlayerName( source ), 225, 0, 0 )
+						createNewAdminDxMessage(getPlayerName(attacker).." possibly deathmatched "..getPlayerName(source), 225, 0, 0)
 					end
 				end
 			end
