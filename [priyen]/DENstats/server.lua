10504834bc7c66f76857c665de52553ea34d5e54
nokizorque <lewis.watson@outlook.com>
2015-08-29 15:19:21 +1000

    [DENstats] Changed the sync method
    - Account data is now synced when it is set as opposed to when the
    player disconnects. Should add a setting for this.
    - Optimized the code to work better and require less querying to the
    database.
    - Other little cleanup bits and pieces.

--- [priyen]/DENstats/server.lua
+++ [priyen]/DENstats/server.lua
@@ -1,4 +1,4 @@
-local db = exports.DENmysql:getConnection()
+db = exports.DENmysql:getConnection()
 local statsTable = {}
 local changed = {}
 local checkSync = {
@@ -29,7 +29,7 @@ local checkSync = {
 addEvent("onServerPlayerLogin")
 addEventHandler("onServerPlayerLogin", root,
 	function ()
-		dbQuery(getDataCB, {source}, db, "SELECT * FROM playerstats WHERE userid=? LIMIT 1", exports.server:getPlayerAccountID(source))
+		dbQuery(getDataCB, {source}, db, "SELECT * FROM `playerstats` WHERE `userid`=? LIMIT 1", exports.server:getPlayerAccountID(source))
 	end
 )
 
@@ -41,10 +41,10 @@ function getDataCB(qh, source)
 		userData = userData[1]
 		statsTable[source] = userData
 		if userData == nil then return end
-		for stat,value in pairs(userData) do
+		for stat, value in pairs(userData) do
 			changed[source][stat] = false
 			if checkSync[stat] == nil or checkSync[stat] ~= false then
-				for k,v in pairs(getElementsByType("player")) do
+				for k, v in pairs(getElementsByType("player")) do
 					--triggerClientEvent(v,"onSyncPlayerStats",v, statsTable[source], source)
 					triggerClientEvent(v, "onSyncSpecific", v, stat, value, source)
 				end
@@ -54,88 +54,93 @@ function getDataCB(qh, source)
 end
 
 -- Function to set a player stat
-function getPlayerAccountData(pE, data)
-	local userID = exports.server:getPlayerAccountID(pE)
-	if isElement(pE) == true and type(tonumber(userID)) == "number" then
-		if (statsTable[pE]) then
-			if (data == 'table') then
-				return statsTable[pE]
-			else
-				return statsTable[pE][data]
-			end
+function getPlayerAccountData(plr, data)
+	if (not plr) or (not data) then return false end
+	if (not isElement(plr)) or (not exports.server:isPlayerLoggedIn(plr)) then return false end
+	
+	local userID = exports.server:getPlayerAccountID(plr)
+	-- If the player is in the table
+	if (statsTable[plr]) then
+		outputDebugString("plr in table")
+		if (data == "table") then
+			return statsTable[plr]
 		else
-			if (data == 'table') then
-				return exports.DENmysql:querySingle("SELECT * FROM playerstats WHERE userid=? LIMIT 1", userID)
-			else
-				local t=exports.DENmysql:querySingle("SELECT * FROM playerstats WHERE userid=? LIMIT 1", userID)
-				local res = false
-				if t == nil then return nil end
-				if (t[data]~=nil) then res=t[data] end
-				return res
-			end
+			return statsTable[plr][data]
 		end
+		
+	-- The player isn't in the table
 	else
-		return false
+		if (data == "table") then
+			return exports.DENmysql:querySingle("SELECT * FROM `playerstats` WHERE `userid`=? LIMIT 1", userID) or nil
+		else
+			return exports.DENmysql:querySingle("SELECT `??` FROM `playerstats` WHERE `userid`=? LIMIT 1", data, userID)[data] or nil
+		end
 	end
 end
 
 -- Function that sets a player stat
-function setPlayerAccountData (pE, data, value)
- 	if isElement(pE) then
-		local userID = exports.server:getPlayerAccountID(pE)
-		--if (userID) and (exports.DENmysql:exec("UPDATE playerstats SET `??`=? WHERE userid=?", data, value, userID)) then
-		if (userID) then
-			--[[
-			if data == "paramedic" then
-				outputDebugString("SAVING    para "..value.."")
-			end
-			--]]
-			if (statsTable[pE]) then
-				statsTable[pE][data] = value
-				changed[pE][data] = true 
-			end
-			for k, v in pairs(getElementsByType("player")) do
-				triggerClientEvent(v, "onSyncSpecific", v, data, value, pE)
-				--triggerClientEvent(v, "onSyncPlayerStats", v, statsTable[pE], pE)
-			end
-			return true
-		else
-			return false
-		end
-	else
-		return false
+function setPlayerAccountData(plr, data, value)
+	-- Preliminary checks
+ 	if (not isElement(plr)) then return false end
+	local userID = exports.server:getPlayerAccountID(plr)
+	if (not userID) then return false end
+	
+	-- Change it in our table
+	if (statsTable[plr]) then
+		statsTable[plr][data] = value
+		changed[plr][data] = true
 	end
+	
+	-- This part here is an alternative to the silly 5 second infinite timer. This should
+	-- Sync it to the database
+	exports.DENmysql:exec("UPDATE `playerstats` SET `??`=? WHERE `userid`=?", data, value, userID)
+	-- Callback and update the data
+	dbQuery(getDataCB, {plr}, db, "SELECT * FROM `playerstats` WHERE `userid`=? LIMIT 1", userID)
+	
+	-- Since we're already syncing it in the callback function, this isn't needed. Keep it here anyway.
+	--[[
+	for k, v in pairs(getElementsByType("player")) do
+		triggerClientEvent(v, "onSyncSpecific", v, data, value, plr)
+		--triggerClientEvent(v, "onSyncPlayerStats", v, statsTable[plr], plr)
+	end
+	--]]
+	return true
 end
 
 -- Function that creates a column
 function createAccountDataRow(aColumn, aType)
 	if (column) and (aType) and (not exports.DENmysql:doesColumnExist(aTable, column)) then
 		exports.DENmysql:exec("ALTER TABLE `playerstats` ADD `??` ??", aColumn, aType)
-		return true
 	else
 		return false
 	end
+	return true
 end
 
+-- No wonder CSG was so laggy and had about 7000 SQL queries every 5 minutes
+-- As far as I see this, it's useless as the data is already cached in a table
+--[[
 setTimer(
 	function()
 		for k, v in pairs(getElementsByType("player")) do
-			if exports.server:isPlayerLoggedIn(v) == true then
+			if (exports.server:isPlayerLoggedIn(v)) then
 				dbQuery(getDataCB, {v}, db, "SELECT * FROM `playerstats` WHERE `userid`=? LIMIT 1", exports.server:getPlayerAccountID(v))
 			end
 		end
 	end, 5000, 1
 )
+--]]
 
 addEventHandler("onPlayerQuit", root,
 	function ()
 		if statsTable[source] == nil then return end
-		local userid = exports.server:getPlayerAccountID(source)
-		for k,v in pairs(statsTable[source]) do
+		--[[
+		for k, v in pairs(statsTable[source]) do
 			if (changed[source][k]) then
 				exports.DENmysql:exec("UPDATE `playerstats` SET "..k.."=? WHERE `userid`=?", v, userid)
 			end
 		end
+		--]]
 		statsTable[source] = nil
 		changed[source] = nil
 	end
