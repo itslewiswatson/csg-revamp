131beb36c3c05a08757a4e6340b6b0c2dca20551
nokizorque <lewis.watson@outlook.com>
2015-08-29 15:25:04 +1000

    [CSGfish] Removed a lot of useless shit

--- [priyen]/CSGfish/shadertest/client.lua
+++
@@ -1,6 +0,0 @@
-texShader = dxCreateShader ( "texreplace.fx" )
-moonTexture = dxCreateTexture("pic.png") -- Replace the filename to your image's file obviously
-
-dxSetShaderValue(texShader,"gTexture",moonTexture)
-
-engineApplyShaderToWorldTexture(texShader,"billbrd01_lan")
